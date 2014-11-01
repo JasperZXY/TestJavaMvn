a9a9d7dbbe04e70e0d5cca9331b5e4d3e762f380
JasperZXY <jasperzxy@gmail.com>
2014-11-01 21:12:54 +0800

    add some redis operation

---
+++ src/main/java/com/jasper/mvntest/NoSQL/redis/RedisConstant.java
@@ -0,0 +1,8 @@
+package com.jasper.mvntest.NoSQL.redis;
+
+public class RedisConstant {
+	public final static String IP = "192.168.244.130";
+	public final static int PORT = 6379;
+	public final static String PASSWORD = "123456";
+
+}
