7500e40f7e6cef06f255b71762ed5b72de2a0915
Jasper Zhong <JasperZXY@gmail.com>
2017-03-26 22:06:37 +0800

    add RocketMQ example

---
+++ src/main/java/com/jasper/mvntest/mq/rocketmq/alibaba/Config.java
@@ -0,0 +1,5 @@
+package com.jasper.mvntest.mq.rocketmq.alibaba;
+
+public abstract class Config {
+    public static final String ADDR = "127.0.0.1:10911";
+}
---
+++ src/main/java/com/jasper/mvntest/mq/rocketmq/apache/transaction/MyService.java
@@ -0,0 +1,41 @@
+package com.jasper.mvntest.mq.rocketmq.apache.transaction;
+
+import org.apache.rocketmq.client.producer.LocalTransactionState;
+import org.apache.rocketmq.common.message.Message;
+
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MyService {
+    private ConcurrentHashMap<String, LocalTransactionState> stateMap = new ConcurrentHashMap<>();
+
+    private String getMessageKey(Message message) {
+        return message.getKeys();
+    }
+
+    public LocalTransactionState getState(Message message) {
+        return stateMap.get(getMessageKey(message));
+    }
+
+    public LocalTransactionState execute(Message message) {
+        System.out.println(new String(message.getBody()));
+        LocalTransactionState state;
+        switch (new Random().nextInt(4)) {
+            case 0:
+                state = LocalTransactionState.COMMIT_MESSAGE;
+                stateMap.put(getMessageKey(message), state);
+                return state;
+            case 1:
+                state = LocalTransactionState.ROLLBACK_MESSAGE;
+                stateMap.put(getMessageKey(message), state);
+                return state;
+            case 2:
+                state = LocalTransactionState.UNKNOW;
+                stateMap.put(getMessageKey(message), state);
+                return state;
+        }
+
+        System.out.println("throw new RuntimeException");
+        throw new RuntimeException("unknow runtime exception");
+    }
+}
