4452d22c226ff73fecbaaa4638c6f29840abf5c4
zhongxianyao <jasperzxy@gmail.com>
2020-04-23 22:43:06 +0800

    加注释

--- src/main/java/com/zxy/demo/jmh/JmhTest.java
+++ src/main/java/com/zxy/demo/jmh/JmhTest.java
@@ -26,6 +26,17 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
  * http://openjdk.java.net/projects/code-tools/jmh/
  * http://hg.openjdk.java.net/code-tools/jmh
  * https://immutables.github.io/apt.html
+ *
+ * BenchmarkMode，使用模式，默认是Mode.Throughput，表示吞吐量,
+ * 其他参数还有AverageTime，表示每次执行时间，SampleTime表示采样时间，
+ * SingleShotTime表示只运行一次，用于测试冷启动消耗时间，All表示统计前面的所有指标
+ * Warmup 配置预热次数，默认是每次运行1秒，运行10次
+ * Measurement 配置执行次数，本例是一次运行5秒，总共运行3次。在性能对比时候，
+ * 采用默认1秒即可，如果我们用jvisualvm做性能监控，我们可以指定一个较长时间运行。
+ * Threads 配置同时起多少个线程执行，默认值是
+ * Runtime.getRuntime().availableProcessors()
+ * Fork，代表启动多个单独的进程分别测试每个方法，我们这里指定为每个方法启动一个进程。
+ * OutputTimeUnit 统计结果的时间单元，这个例子TimeUnit.NANOSECONDS，我们在运行后会看到输出结果是统计每秒的吞吐量
  */
 @BenchmarkMode({Mode.AverageTime, Mode.Throughput}) // 测试方法平均执行时间跟吞吐量
 @OutputTimeUnit(TimeUnit.NANOSECONDS) // 输出结果的时间粒度为纳秒
