86b1764688c59b40958fe16218e7004c35e71a21
zxy <240995135@qq.com>
2021-09-12 20:04:26 +0800

    用 Jetty 跑一个最简单是 Servlet，方便后面演示其他东西

---
+++ src/main/java/com/zxy/demo/jetty/v1/App.java
@@ -0,0 +1,23 @@
+package com.zxy.demo.jetty.v1;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+/**
+ * 用 Jetty 跑一个最简单是 Servlet，方便后面演示其他东西
+ */
+public class App {
+
+    public static void main(String[] args) throws Throwable {
+        Server server = new Server(8080);
+
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+
+        handler.addServletWithMapping(HelloServlet.class, "/*");
+
+        server.start();
+        server.join();
+    }
+
+}
---
+++ src/main/java/com/zxy/demo/jetty/v1/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.zxy.demo.jetty.v1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("Hello Servlet");
+    }
+}
