c51a4fcdeee579365d4344da929e88fe8228b1ae
zxy <jasperzxy@gmail.com>
2021-09-14 15:48:20 +0800

    改成依赖 jetty-webapp，可以运行就启动Servlet

--- src/main/java/com/zxy/demo/jetty/v1/App.java
+++ src/main/java/com/zxy/demo/jetty/v1/App.java
@@ -1,22 +1,26 @@
 package com.zxy.demo.jetty.v1;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 
 /**
  * 用 Jetty 跑一个最简单是 Servlet，方便后面演示其他东西
  *
- * 依赖 jetty-distribution
+ * 依赖 jetty-webapp，可以运行就启动Servlet
+ * 之前是jetty-distribution，没法运行就启动Servlet
  */
 public class App {
 
     public static void main(String[] args) throws Throwable {
         Server server = new Server(8080);
 
-        ServletHandler handler = new ServletHandler();
-        server.setHandler(handler);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
 
-        handler.addServletWithMapping(HelloServlet.class, "/*");
+        context.addServlet(new ServletHolder(new HelloServlet()), "/hello");
 
         server.start();
         server.join();
--- src/main/java/com/zxy/demo/spring/mvp/App.java
+++ src/main/java/com/zxy/demo/spring/mvp/App.java
@@ -1,8 +1,11 @@
 package com.zxy.demo.spring.mvp;
 
 import com.zxy.demo.spring.mvp.core.ZxyDispatcherServlet;
+
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 
 /**
  * 一个精简版的Spring-MVC模仿版本，只需依赖Jetty + Servlet
@@ -12,10 +15,13 @@ public class App {
     public static void main(String[] args) throws Throwable {
         Server server = new Server(8080);
 
-        ServletHandler handler = new ServletHandler();
-        server.setHandler(handler);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
 
-        handler.addServletWithMapping(ZxyDispatcherServlet.class, "/*");
+        ServletHolder holder = new ServletHolder(new ZxyDispatcherServlet());
+        holder.setInitOrder(1);
+        context.addServlet(holder, "/*");
 
         server.start();
         server.join();
